ad4523f7846248765d0e866cfce429879592922d
Evan Thomas <ethomas93@gatech.edu>
2025-09-17 22:04:41 -0400

    example image output

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Mandelbrot Zoom
 Program to generate a visualization of the mandelbrot set, written in C++.
 
-
+# dependencies
+This project uses stb_image_write for image saving (already included in this repository).
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
-int main() {
+#include "stb_image_write.h"
 
+constexpr int IMAGE_WIDTH = 1920;
+constexpr int IMAGE_HEIGHT = 1080;
+
+int main() {
+	unsigned char* image = new unsigned char[IMAGE_WIDTH*IMAGE_HEIGHT*4];
+	for (int i = 0; i < IMAGE_HEIGHT; ++i) {
+		for (int j = 0; j < IMAGE_WIDTH; ++j) {
+			image[(j+IMAGE_WIDTH*i)*4] = static_cast<unsigned char>(static_cast<float>(j)/static_cast<float>(IMAGE_WIDTH)*255.0f);
+			image[(j+IMAGE_WIDTH*i)*4+1] = static_cast<unsigned char>(static_cast<float>(i)/static_cast<float>(IMAGE_HEIGHT)*255.0f);
+			image[(j+IMAGE_WIDTH*i)*4+3] = 255;
+		}
+	}
+	stbi_write_bmp("test.bmp", IMAGE_WIDTH, IMAGE_HEIGHT, 4, image);
+	delete[] image;
 }
